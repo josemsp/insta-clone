50c24bace8861655effafa39748d3bf39c32d300
JSalper <jose_salazar_@outlook.com>
2024-07-29 00:35:33 -0600

    CI - CD Added

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === 'production' ? '/netflix-clone/' : '/',
+    base: mode === 'production' ? '/insta-clone/' : '/', 
     plugins: [
       react(),
       svgr()
