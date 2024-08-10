1a04b72803ce6679af625f82b9a78847e4072b2c
JSalper <jose_salazar_@outlook.com>
2024-08-09 23:06:05 -0600

    sqs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,18 @@ import { getStorage } from "firebase/storage";
 // import { seedDatabase } from "../seed";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_APP_FIREBASE_API_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_FIREBASE_API_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_FIREBASE_API_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_API_MESSAGING,
-  appId: import.meta.env.VITE_APP_FIREBASE_API_APP_ID
+  // apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  // authDomain: import.meta.env.VITE_APP_FIREBASE_API_AUTH_DOMAIN,
+  // projectId: import.meta.env.VITE_APP_FIREBASE_API_PROJECT_ID,
+  // storageBucket: import.meta.env.VITE_APP_FIREBASE_API_STORAGE_BUCKET,
+  // messagingSenderId: import.meta.env.VITE_APP_FIREBASE_API_MESSAGING,
+  // appId: import.meta.env.VITE_APP_FIREBASE_API_APP_ID
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY || Cypress.env('VITE_APP_FIREBASE_API_KEY'),
+  authDomain: import.meta.env.VITE_APP_FIREBASE_API_AUTH_DOMAIN || Cypress.env('VITE_APP_FIREBASE_API_AUTH_DOMAIN'),
+  projectId: import.meta.env.VITE_APP_FIREBASE_API_PROJECT_ID || Cypress.env('VITE_APP_FIREBASE_API_PROJECT_ID'),
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_API_STORAGE_BUCKET || Cypress.env('VITE_APP_FIREBASE_API_STORAGE_BUCKET'),
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_API_MESSAGING || Cypress.env('VITE_APP_FIREBASE_API_MESSAGING'),
+  appId: import.meta.env.VITE_APP_FIREBASE_API_APP_ID || Cypress.env('VITE_APP_FIREBASE_API_APP_ID')
 };
 
 // Initialize Firebase
