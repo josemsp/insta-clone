85d58392baa37e97fca2b587a84603d4c7646ca8
JSalper <jose_salazar_@outlook.com>
2024-08-09 20:29:05 -0600

    sqs

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,13 @@ export default defineConfig({
     },
     baseUrl: 'http://localhost:5173/#/',
     supportFile: 'cypress/support/e2e.ts',
+    env: {
+      VITE_APP_FIREBASE_API_KEY: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+      VITE_APP_FIREBASE_API_AUTH_DOMAIN: import.meta.env.VITE_APP_FIREBASE_API_AUTH_DOMAIN,
+      VITE_APP_FIREBASE_API_PROJECT_ID: import.meta.env.VITE_APP_FIREBASE_API_PROJECT_ID,
+      VITE_APP_FIREBASE_API_STORAGE_BUCKET: import.meta.env.VITE_APP_FIREBASE_API_STORAGE_BUCKET,
+      VITE_APP_FIREBASE_API_MESSAGING: import.meta.env.VITE_APP_FIREBASE_API_MESSAGING,
+      VITE_APP_FIREBASE_API_APP_ID: import.meta.env.VITE_APP_FIREBASE_API_APP_ID
+    },
   },
 });
